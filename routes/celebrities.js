1e76259a70eee306d6528af3a3b0a13c84aa502a
kevmhughes <kevhughes24@hotmail.com>
2024-10-17 02:53:53 +0200

    Complete iteration 3.

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Celebrity = require("../models/celebrity")
 
 /* GET celebrities page */
@@ -7,15 +7,35 @@ router.get('/celebrities', async (req, res, next) => {
 
     try {
         // Fetch all celebrities from the database
-        const allCelebrities = await Celebrity.find();    
+        const allCelebrities = await Celebrity.find();
 
         // Render the 'celebrities/index' view and pass the data
-        res.render('celebrities/index', { allCelebrities }); 
+        res.render('celebrities/index', { allCelebrities });
     } catch (error) {
         console.error('Error fetching celebrities:', error);
 
-        // Pass the error to the next middleware (could be an error handler)
-        next(error); // You can also render a custom error page if preferred
+        // Pass the error to the next middleware
+        next(error); 
+    }
+
+});
+
+/* GET celebrity details page */
+router.get('/celebrities/:id', async (req, res, next) => {
+
+    try {
+        // retrieve id from params 
+        const { id } = req.params
+        // find the celebrity with the id
+        const oneCelebrity = await Celebrity.findById(id);
+
+        // Render the 'celebrities/show' view and pass the data
+        res.render('celebrities/show', { oneCelebrity });
+    } catch (error) {
+        console.error('Error fetching celebrity:', error);
+
+        // Pass the error to the next middleware
+        next(error); 
     }
 
 });
